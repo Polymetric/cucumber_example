b3a0e5698f210376aed1834de2d68b2819d8268c
Polymetric <austin.is.paxton@gmail.com>
2016-01-07 16:02:29 -0600

    added driver for IE, requires some setup on the remote

--- features/support/capybara.rb
+++ features/support/capybara.rb
@@ -1,7 +1,22 @@
 require 'capybara/cucumber'
+require 'selenium-webdriver'
 
 Capybara.register_driver :chrome do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome)
 end
 
-Capybara.default_driver = :chrome
+Capybara.register_driver :ie do |app|
+  Capybara::Selenium::Driver.new(app,
+    :browser => :remote,
+    :url => "http://192.168.56.101:4444/wd/hub",
+    :desired_capabilities => :internet_explorer)
+end
+
+Capybara.register_driver :edge do |app|
+  Capybara::Selenium::Driver.new(app,
+    :browser => :remote,
+    :url => "http://192.168.56.101:4444/wd/hub",
+    :desired_capabilities => :edge)
+end
+
+Capybara.default_driver = :ie
